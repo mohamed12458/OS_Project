5295c49832acb70de7f8a45a5b66c4449246d941
mo3mr <84670559+mo3mr@users.noreply.github.com>
2024-12-21 20:43:16 +0200

    Mohamed Amr Comment

--- dohaa.cpp
+++ dohaa.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 void main() {
 	int a, b,result;
-	cout << "enter your numbers  " << endl;
+	cout << "enter two numbers  " << endl;
 	cin >> a >> b;
 	result = a + b;
 	cout << "the result is : " << result << endl;
+// Edited By Mohamed Amr Elsabbagh
 }
